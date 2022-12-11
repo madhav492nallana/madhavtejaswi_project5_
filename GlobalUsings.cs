01e44bd8df7593722bd538d95d34c4715b3cb1e9
madhav492nallana <117795636+madhav492nallana@users.noreply.github.com>
2022-12-11 17:44:30 -0600

    Add files via upload

---
+++ GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System.Globalization;
+global using SkiaSharp;
+global using SkiaSharp.Views.Maui.Controls.Hosting;
+global using CommunityToolkit.Mvvm.Input;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using myBMI.Views;
+global using myBMI.ViewModels;
